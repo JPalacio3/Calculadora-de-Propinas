bdb1fc42080725a0238aaef88f6222673fd3ce90
JPalacio3 <joelpalacio630@gmail.com>
2025-02-20 17:33:06 -0600

    Refactor useOrder hook for improved readability and consistency

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,44 +1,42 @@
-import { useState } from "react"
-import { MenuItem, OrderItem } from "../types"
+import { useState } from "react";
+import { MenuItem, OrderItem } from "../types";
 
+export function useOrder() {
+	const [order, setOrder] = useState<OrderItem[]>([]);
+	const [tip, setTip] = useState(0);
 
-export function useOrder () {
+	const addItem = (item: MenuItem) => {
+		const itemExist = order.find((orderItem) => orderItem.id === item.id);
 
-    const [ order, setOrder ] = useState<OrderItem[]>( [] )
-    const [ tip, setTip ] = useState( 0 )
+		if (itemExist) {
+			const updatedOrden = order.map((orderItem) =>
+				orderItem.id === item.id
+					? { ...orderItem, quantity: orderItem.quantity + 1 }
+					: orderItem
+			);
 
-    const addItem = ( item: MenuItem ) => {
+			setOrder(updatedOrden);
+		} else {
+			const newItem = { ...item, quantity: 1 };
+			setOrder([...order, newItem]);
+		}
+	};
 
-        const itemExist = order.find( orderItem => orderItem.id === item.id )
+	const removeItem = (id: MenuItem["id"]) => {
+		setOrder(order.filter((item) => item.id !== id));
+	};
 
-        if ( itemExist ) {
-            const updatedOrden = order.map( orderItem => orderItem.id === item.id
-                ? { ...orderItem, quantity: orderItem.quantity + 1 } : orderItem )
+	const placeOrder = () => {
+		setOrder([]);
+		setTip(0);
+	};
 
-            setOrder( updatedOrden )
-
-        } else {
-            const newItem = { ...item, quantity: 1 }
-            setOrder( [ ...order, newItem ] )
-        }
-    }
-
-    const removeItem = ( id: MenuItem[ 'id' ] ) => {
-        setOrder( order.filter( item => item.id !== id ) )
-    }
-
-    const placeOrder = () => {
-        setOrder( [] )
-        setTip( 0 )
-    }
-
-    return {
-        order,
-        addItem,
-        tip,
-        setTip,
-        removeItem,
-        placeOrder
-
-    }
+	return {
+		order,
+		addItem,
+		tip,
+		setTip,
+		removeItem,
+		placeOrder,
+	};
 }
