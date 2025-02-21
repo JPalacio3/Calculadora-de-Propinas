1f7cb72fd9edfca26362ed48c626dc8e23cd6de4
JPalacio3 <joelpalacio630@gmail.com>
2025-02-20 18:51:58 -0600

    Implementar manejo de estado de pedidos con useReducer y refactorizar MenuItem para usar dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,56 @@
-import MenuItem from '../src/components/MenuItem';
-import OrderContents from './components/OrderContents';
-import OrderTotals from './components/OrderTotals';
-import TipPercentageForm from './components/TipPercentageForm';
-import { menuItems } from './data/db';
-import { useOrder } from './hooks/useOrder';
+import { useReducer } from "react";
+import MenuItem from "../src/components/MenuItem";
+import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
+import { menuItems } from "./data/db";
+import { useOrder } from "./hooks/useOrder";
+import { orderReducer, initialState } from "./reducers/order-reducer";
 
 function App() {
-    const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder();
+	const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder();
+	const [state, dispatch] = useReducer(orderReducer, initialState);
 
-    return (
-        <>
-            <header className="bg-teal-400 py-5">
-                <h1 className="text-center text-4xl font-semibold">
-                    Calculadora de Propinas y Consumo
-                </h1>
-            </header>
+	return (
+		<>
+			<header className="bg-teal-400 py-5">
+				<h1 className="text-center text-4xl font-semibold">
+					Calculadora de Propinas y Consumo
+				</h1>
+			</header>
 
-            <main className="max-w-7xl mx-auto my-20 grid md:grid-cols-2 items-center">
-                <div className="p-5">
-                    <h2 className="text-4xl font-bold">Menú</h2>
-                    <div className="space-y-1 mt-10">
-                        {menuItems.map((item) => (
-                            <MenuItem key={item.id} item={item} addItem={addItem} />
-                        ))}
-                    </div>
-                </div>
+			<main className="max-w-7xl mx-auto my-20 grid md:grid-cols-2 items-center">
+				<div className="p-5">
+					<h2 className="text-4xl font-bold">Menú</h2>
+					<div className="space-y-1 mt-10">
+						{menuItems.map((item) => (
+							<MenuItem key={item.id} item={item} dispatch={dispatch} />
+						))}
+					</div>
+				</div>
 
-                <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-                    {order.length ? (
-                        <>
-                            <h2 className="font-black text-4xl text-center">Consumo</h2>
+				<div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+					{state.order.length ? (
+						<>
+							<h2 className="font-black text-4xl text-center">Consumo</h2>
 
-                            <OrderContents order={order} removeItem={removeItem} />
+							<OrderContents order={state.order} removeItem={removeItem} />
 
-                            <TipPercentageForm setTip={setTip} tip={tip} />
+							<TipPercentageForm setTip={setTip} tip={tip} />
 
-                            <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
-                        </>
-                    ) : (
-                        <p className="text-center ">La Orden está vacía</p>
-                    )}
-                </div>
-            </main>
-        </>
-    );
+							<OrderTotals
+								order={state.order}
+								tip={tip}
+								placeOrder={placeOrder}
+							/>
+						</>
+					) : (
+						<p className="text-center ">La Orden está vacía</p>
+					)}
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,18 +1,21 @@
-import { formatCurrency } from '../helpers';
-import type { MenuItem } from '../types';
+import { Dispatch } from "react";
+import { formatCurrency } from "../helpers";
+import type { MenuItem } from "../types";
+import { OrderActions } from "../reducers/order-reducer";
 
 type MenuItemProps = {
-    item: MenuItem;
-    addItem: (item: MenuItem) => void;
+	item: MenuItem;
+	dispatch: Dispatch<OrderActions>;
 };
 
-export default function MenuItem({ item, addItem }: MenuItemProps) {
-    return (
-        <button
-            className="border-2 border-teal-400 w-full p-2 flex justify-between hover:bg-teal-100"
-            onClick={() => addItem(item)}>
-            <p>{item.name}</p>
-            <p className="font-bold">{formatCurrency(item.price)}</p>
-        </button>
-    );
+export default function MenuItem({ item, dispatch }: MenuItemProps) {
+	return (
+		<button
+			className="border-2 border-teal-400 w-full p-2 flex justify-between hover:bg-teal-100"
+			onClick={() => dispatch({ type: "add-item", payload: { item } })}
+		>
+			<p>{item.name}</p>
+			<p className="font-bold">{formatCurrency(item.price)}</p>
+		</button>
+	);
 }
